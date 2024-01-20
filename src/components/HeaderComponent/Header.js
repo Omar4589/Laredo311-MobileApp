0058fdc2f96e6999a4fa640a801fcd71225ee988
Omar4589 <omar4589@hotmail.com>
2024-01-20 01:13:27 -0600

    removes icon text from header

--- src/components/HeaderComponent/Header.js
+++ src/components/HeaderComponent/Header.js
@@ -5,9 +5,7 @@ const Header = ({appName}) => {
   return (
     <View className="flex-row justify-between items-center h-16 px-6 bg-blue-500">
       <Text className="text-slate-100 text-xl">{appName}</Text>
-      <TouchableOpacity>
-        <Text>Icon</Text>
-      </TouchableOpacity>
+      <TouchableOpacity />
     </View>
   );
 };
