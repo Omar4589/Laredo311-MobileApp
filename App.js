ab104547d47348d8a95f07086e36d127190bee6e
Omar4589 <omar4589@hotmail.com>
2024-01-23 01:15:36 -0600

    creates Drawer navigator for menu tab

--- App.js
+++ App.js
@@ -1,14 +1,6 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {StatusBar} from 'react-native';
 import Navigation from './src/navigation/Navigation';
 
 function App() {
---
+++ src/navigation/Item1.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+const Item1Screen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Item 1 Screen</Text>
+      <Button
+        title="Go Back to Menu"
+        onPress={() => navigation.goBack()} // Use navigation to go back
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+  text: {
+    fontSize: 20,
+    marginBottom: 20,
+  },
+});
+
+export default Item1Screen;
