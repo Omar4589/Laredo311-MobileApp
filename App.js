8fd9dcf609ceb7af17cac5992e79219462769e8a
Omar4589 <omar4589@hotmail.com>
2024-01-24 21:05:57 -0600

    sets up apollo client

--- App.js
+++ App.js
@@ -1,13 +1,46 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import {setContext} from '@apollo/client/link/context';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
 import {StatusBar} from 'react-native';
 import Navigation from './src/navigation/Navigation';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+// over wifi with device 192.168.16.219:3001/graphql
+
+const authLink = setContext(async (_, {headers}) => {
+  // Get the authentication token from storage if it exists
+  const token = await AsyncStorage.getItem('id_token'); // Or AsyncStorage
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
-      <StatusBar />
-      <Navigation />
+      <ApolloProvider client={client}>
+        <StatusBar />
+        <Navigation />
+      </ApolloProvider>
     </>
   );
 }
