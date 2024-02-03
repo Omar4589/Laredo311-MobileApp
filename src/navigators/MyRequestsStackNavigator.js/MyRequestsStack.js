117722cfad51c7463c0cd806c9485287b20531e2
Omar4589 <omar4589@hotmail.com>
2024-02-03 00:17:15 -0600

    adds statusText function that renders requests status in headerRight

--- src/navigators/MyRequestsStackNavigator.js/MyRequestsStack.js
+++ src/navigators/MyRequestsStackNavigator.js/MyRequestsStack.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {useNavigation} from '@react-navigation/native';
+import {View, Text} from 'react-native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import MyRequestsScreen from '../../screens/MyRequestsScreen/MyRequestsScreen';
 import ViewRequestScreen from '../../screens/ViewRequestScreen/ViewRequestScreen';
@@ -9,6 +10,25 @@ import {useAlertsContext} from '../../contexts/AlertsContext';
 const MyRequests = createNativeStackNavigator();
 
 const MyRequestsStack = ({}) => {
+  const statusText = route => {
+    return (
+      <View
+        className={`${
+          route.params.requestStatus === 'Canceled'
+            ? 'bg-red-600'
+            : route.params.requestStatus === 'Completed'
+            ? 'bg-slate-800'
+            : 'bg-green-500'
+        }  px-3 rounded-md`}>
+        <Text
+          className="
+      text-base text-slate-100 font-exo font-bold">
+          {route.params.requestStatus}
+        </Text>
+      </View>
+    );
+  };
+
   const {toggleAlertsBox} = useAlertsContext();
   return (
     <MyRequests.Navigator screenOptions={{headerShown: false}}>
@@ -23,8 +43,13 @@ const MyRequestsStack = ({}) => {
           headerBackTitleVisible: false,
           headerTintColor: '#f1f5f9',
           headerTitle: `Request: #${route.params.requestNumber}`,
+          headerRight: () => statusText(route),
           headerStyle: {backgroundColor: '#2563ea'},
-          headerTitleStyle: {color: '#f1f5f9'},
+          headerTitleStyle: {
+            color: '#f1f5f9',
+            fontFamily: 'exo',
+            fontWeight: '600',
+          },
         })}
         name="ViewRequest"
         component={ViewRequestScreen}
